c3afff7e1414b10e4006275116ced82bb0da53a4
Zaerogurl <114050880+Zaerogurl@users.noreply.github.com>
2022-09-28 08:17:13 +0300

    now game REPEATS

--- game.py
+++ game.py
@@ -1,13 +1,21 @@
-human_turn = input('Input human turn: ')
-computer_turn = input ('input computer turn: ')
+import random
+turns = ["rock", "paper", "scissors" ]
 
-if human_turn == computer_turn :
-    print ('draw!')
-elif human_turn == 'r' and computer_turn == 's':
-    print('Human wins!')
-elif human_turn == 'p' and computer_turn == 'r':
-    print('Human wins!')
-elif human_turn == 's' and computer_turn == 'p':
-    print('Human wins!')
-else:
-        print('computer wins!')
+
+while(True):
+    human_turn = input("enter your turn,or type exit: ")
+    computer_turn = random.choice(turns)
+
+    if human_turn == 'exit':
+        break
+    print(f'Human:{human_turn} vs. {computer_turn}')
+    if human_turn == computer_turn:
+        print("Draw!")
+    elif human_turn == "rock" and computer_turn == "scissors":
+        print("Human wins!")
+    elif human_turn == "paper" and computer_turn == "rock":
+        print("Human wins!")
+    elif human_turn == "scissors" and computer_turn == "paper":
+        print("Human wins!")
+    else:
+        print("Computer wins!")
